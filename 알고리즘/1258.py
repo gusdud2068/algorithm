94722eff291d7f49a3219b16008f2abe2a9fc8c0
gusdud2068 <gusdud2068@gmail.com>
2022-10-19 21:45:02 +0900

    221019

--- 알고리즘/1258.py
+++ 알고리즘/1258.py
@@ -6,21 +6,21 @@ for tc in range(1,1+T):
     n=int(input())
     lst=[list(map(int,input().split())) for _ in range(n)]
 
-    print(lst)
+    #print(lst)
 
     empty=[]
-    sum2=0
+
+
     for i in range(n):
-        sum1 = 0
         for j in range(n):
-            if lst[i][j]!=0:
-                sum1+=1
-        if lst[i][j] != 0:
-            sum2 += 1
-    empty.append(sum1)
-    empty.append(sum2)
+            if lst[i-1][j] ==  0   and lst[]
 
 
-    print(empty)
+
+
+    print(lst)
+
+
+
 
 
