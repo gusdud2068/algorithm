8f2840fdf6799829cd2440ad5f868aade422dcb1
gusdud2068 <gusdud2068@gmail.com>
2022-11-03 17:59:01 +0900

    221103

---
+++ vue-cli/src/components/MyComponentItem.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h3>하위커포넌트래용</h3>
+        <p> {{staticProps}} </p>
+        <p> {{myProps}} </p>
+        <button @click="childToParent">클릭!</button>
+    </div>
+  
+</template>
+
+
+<script>
+
+
+export default {
+    name : 'MyComponentItem',
+    props : {
+        staticProps : String,
+        myProps : String,
+    },
+    methods: {
+        childToParent: function(){
+            this.$emit('child-to-parent',"나는 자식이 보낸 데이터다")
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
